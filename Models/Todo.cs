698ef53748d08f78491bdef309d1a064e6d5cfb2
htwright <harrisontwright@gmail.com>
2017-09-27 15:15:37 -0500

    reading and populating data from remote psql

--- Models/Todo.cs
+++ Models/Todo.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Npgsql;
 using System.Data.SqlClient;
 namespace Todo_Mvc.Models
@@ -30,6 +31,39 @@ namespace Todo_Mvc.Models
         }
 
         public static System.Collections.Generic.List<Todo> GetAll(){
+
+          string ConnString = "Host=tantor.db.elephantsql.com;Username=whiptylt;Password=uLlB5fEK9y_Q82cNj8daLMRtSzys03jf;Database=whiptylt";
+          using(NpgsqlConnection Conn = new NpgsqlConnection(ConnString)){
+            try{
+              Conn.Open();
+            } catch (Exception e){
+              Console.WriteLine(e.ToString());
+            }
+            List<string> returnList = new List<string>();
+            using (var cmd = new NpgsqlCommand("SELECT * FROM todos", Conn)){
+              // cmd.ExecuteNonQuery();
+              NpgsqlDataReader dr = cmd.ExecuteReader();
+              while(dr.Read()) {
+                  returnList.Add(dr.GetString(1));
+                  
+                }
+                // Console.WriteLine(dr[1].GetType());
+                // Todo x = new Todo();
+                // Console.WriteLine(dr);
+                // x.Task = dr[1];
+                // GlobalVariables.Todos.Add(x);
+              }
+              Console.WriteLine("done");
+              foreach(string s in returnList){
+                Todo x = new Todo();
+                x.Task = s;
+                GlobalVariables.Todos.Add(x);
+                Console.WriteLine(s);
+              }
+              
+
+            }
+          // }
           // GlobalVariables.foreach()
         Console.WriteLine(GlobalVariables.Todos);
           return GlobalVariables.Todos;
